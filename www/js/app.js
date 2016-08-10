359a434eb8a45c24f50a99b7f64b1bb18ee1dbb0
kimtoto <kimtoto0619@gmail.com>
2016-08-10 20:05:58 +0900

    commit by kyung tae

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,19 @@
+'use strict';
 angular.module('App', ['ionic'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: '/home',
+            templateUrl: 'views/home/home.html',
+        });
+        $urlRouterProvider.otherwise('/home');
+    })
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    });
